f0e1eb6193700cd853fc6c799077c9b9d15b5fcf
Luis Romero <creytuningct@gmail.com>
2020-04-18 15:45:34 -0400

    Nuevo archivo Json de Genesis revisado.

--- lib/data/Define.dart
+++ lib/data/Define.dart
@@ -1,6 +1,6 @@
 Map<int, String> intToBookPath =
 {
-  1: "lib/bible/1_Gn.json",
+  1: "lib/bible/1_Gn_R.json",
   2: "lib/bible/2_Ex.json",
   3: "lib/bible/3_Lv.json",
   4: "lib/bible/4_Nm.json",
