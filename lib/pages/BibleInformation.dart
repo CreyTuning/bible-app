14a91f10caf7fc00f0e9a22c05270da0d1b0dd76
Luis Romero <creytuningct@gmail.com>
2020-04-21 18:47:27 -0400

    Gn 6:22, 5:32

--- lib/pages/BibleInformation.dart
+++ lib/pages/BibleInformation.dart
@@ -24,7 +24,7 @@ class _BibleInformationState extends State<BibleInformation>
             RichText(
               text: TextSpan(
                   text: "Obra de la Iglesia Bautista Bíblica de la Gracia en Monterrey.\n\nSe han invertido años en el proceso de la purificación de la Biblia de Valera 1602.\n\nMuchos creemos que el texto griego neo-testamentario que Dios siempre ha bendecido por los siglos de la iglesia es el texto conocido como el “Texto Recibido”.\n\nSi usted está de acuerdo con este principio, entonces usará una Biblia que lleva el nombre “Reina Valera” (pero no la “Reina Valera Actualizada”, la cual es una Biblia basada en el texto corrupto conocido como el “Texto Alejandrino” o el “Texto Crítico”).\n\nPero, ¿cuál edición de la Reina Valera debemos usar? Hubo muchas revisiones de ella. Principalmente los cristianos evangélicos hemos usado o la Antigua Versión de 1909 o la Revisión de 1960.\n\nLa Revisión de 1865 fue impresa en cantidades grandes hasta como 1950, y últimamente está siendo reimpresa por una iglesia bautista independiente.\n\nTambién la Sociedad Bíblica Trinitaria de Inglaterra hizo una revisión de la Antigua Versión en 2001 que corrigió muchas de las discrepancias en la Antigua Versión de 1909.",
-                  style: TextStyle(fontFamily: 'Roboto', fontSize: 18, color: Color(0xff263238),)
+                  style: Theme.of(context).textTheme.body2
               ),
               textAlign: TextAlign.left,
             ),
@@ -34,7 +34,7 @@ class _BibleInformationState extends State<BibleInformation>
     );
 
     return Container(
-      color: Colors.green,
+      color: Theme.of(context).appBarTheme.color,
       child: SafeArea(
         child: Scaffold(
             body: Scrollbar(
@@ -43,11 +43,7 @@ class _BibleInformationState extends State<BibleInformation>
                   SliverAppBar(
                     forceElevated: true,
                     floating: true,
-                    backgroundColor: Colors.green,
-                     title: Text(
-                      "Valera 1602 Purificada",
-                      style: TextStyle(fontFamily: 'Roboto', fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
+                     title: Text("Valera 1602 Purificada"),
                   ),
 
                   SliverList(
